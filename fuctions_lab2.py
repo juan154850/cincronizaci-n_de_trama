9063e5f6ae93841398f8a6f7946efd232e5849c3
Juan Diego Bermudez Castañeda <juan052115@gmail.com>
2023-10-27 13:22:05 -0500

    Se crea la función para cargar el archivo de texto

---
+++ fuctions_lab2.py
@@ -0,0 +1,7 @@
+def read_file(file_name: str):
+    with open(file_name, "r") as file:
+        return file.read()
+
+def get_frame_sync():
+    
+    pass
---
+++ lab_2_juan_bermudez.py
@@ -0,0 +1,8 @@
+from fuctions_lab2 import read_file
+
+# Archivo con el contenido txt.
+flujoE1 = read_file("flujoE1.txt")
+# Señal de sincronización de trama
+FAS = "0011011"
+
+# Encontramos la sincronización de trama
